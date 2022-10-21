999a7b681d30a2fbd979f457a3e0b1d966a3e50f
Ervin Solano Vargas <esolanov@grupocolono.com>
2022-10-21 15:40:47 -0600

    Agregar archivos de proyecto.

---
+++ src/Clean.Architecture.Core/ProjectAggregate/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clean.Architecture.SharedKernel;
+using Clean.Architecture.SharedKernel.Interfaces;
+
+namespace Clean.Architecture.Core.ProjectAggregate;
+public class Product : EntityBase, IAggregateRoot
+{
+    //El new es para que no se use el id de la interface IAggregateRoot
+    public new int Id { set; get; }
+    //el ? indica que puede ser nulo
+    public string Name { set; get; } = String.Empty;
+    public decimal Price { set; get; }
+    public string Category { set; get; } = String.Empty;
+    public string Manufacturer { set; get; } = String.Empty;
+}
---
+++ src/Clean.Architecture.Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clean.Architecture.Core.ProjectAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Clean.Architecture.Infrastructure.Data.Config;
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Product");
+        builder.HasKey(x => new { x.Id});
+    }
+}
